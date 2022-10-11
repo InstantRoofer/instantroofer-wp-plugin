335cebe3817e2b8eb06e0c8587617466e98c749f
Charles Koehl <charles@etherscapes.com>
2022-10-11 12:28:12 -1000

    feat: stub files

---
+++ instant-roofer.php
@@ -0,0 +1,15 @@
+/**
+* Plugin Name:       Instant Roofer
+* Plugin URI:        https://instantroofer.com/integrations/wordpress-plugin
+* Description:       Embed the Instant Roofer Booking Engine on your WP site.
+* Version:           1.10.3
+* Requires at least: 5.2
+* Requires PHP:      7.2
+* Author:            Charles Koehl
+* License:           GPL v2 or later
+* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+* Update URI:        https://instantroofer.com/integrations/wordpress-plugin
+* Text Domain:       instant-roofer-plugin
+* Domain Path:       /languages
+*/
+
