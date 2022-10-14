39697b721655dc36979630c9d4cbb79c4e38ef93
Charles Koehl <charles@etherscapes.com>
2022-10-14 12:57:44 -1000

    chore: version bump

--- instant-roofer.php
+++ instant-roofer.php
@@ -3,7 +3,7 @@
 * Plugin Name:       Instant Roofer Booking Engine
 * Plugin URI:        https://instantroofer.com/integrations/wordpress-plugin
 * Description:       Embed the Instant Roofer Booking Engine on your WP site.
-* Version:           1.10.21
+* Version:           1.10.22
 * Requires at least: 5.2
 * Requires PHP:      7.2
 * Author:            Charles Koehl
