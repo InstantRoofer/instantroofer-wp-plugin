0010b3bc50c25a229ff77714e56b3fd7aecab96a
Charles Koehl <charles@etherscapes.com>
2022-10-14 13:49:31 -1000

    feat: font color picker

--- js/color-script.js
+++ js/color-script.js
@@ -22,7 +22,7 @@ jQuery(document).ready(function($){
         const input = $(`#${fieldId}`)
         console.log('input id', input.attr('id'))
         const colorButton = input.parent('.wp-picker-container').find('.wp-color-result')
-        console.log('colorButton has class button:', colorButton.hasClass('button'))
+        console.log('colorButton has class wp-color-result:', colorButton.hasClass('wp-color-result'))
         const options = colorPickerFields[fieldId]
         input.wpColorPicker(options);
         if(input.val().length === 0) {
