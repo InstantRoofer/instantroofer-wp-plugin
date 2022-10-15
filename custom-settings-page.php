a924a1302d61a741c628549370ff2dfab3570833
Charles Koehl <charles@etherscapes.com>
2022-10-14 16:29:54 -1000

    chore: debugfeat: font color picker

--- custom-settings-page.php
+++ custom-settings-page.php
@@ -288,7 +288,7 @@ function mw_enqueue_color_picker($hook_suffix)
 {
 // first check that $hook_suffix is appropriate for your admin page
     wp_enqueue_style('wp-color-picker');
-    wp_enqueue_script('color-script-handle', plugins_url('js/color-script.js', __FILE__), array('wp-color-picker'), '1.0.19', true);
+    wp_enqueue_script('color-script-handle', plugins_url('js/color-script.js', __FILE__), array('wp-color-picker'), '1.0.20', true);
 }
 
 
