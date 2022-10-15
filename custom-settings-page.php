1a4fd4c12e05d550ecba90b16b0a8f72abe572c8
Charles Koehl <charles@etherscapes.com>
2022-10-14 14:54:25 -1000

    fix: font families

--- custom-settings-page.php
+++ custom-settings-page.php
@@ -40,7 +40,7 @@ const FONT_FAMILIES = [
 
 const DEFAULTS = array(
     'instantroofer_field_account_id' => '',
-    'instantroofer_field_font_family' => FONT_FAMILIES[0],
+    'instantroofer_field_font_family' => 'Arial, Helvetica Neue, Helvetica, sans-serif',
     'instantroofer_field_width' => 640,
     'instantroofer_field_height' => 690,
     'instantroofer_field_font_color' => '#000000'
