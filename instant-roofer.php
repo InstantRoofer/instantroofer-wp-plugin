b44af0c9e815352893e9d32dbbe08d85f56aaa84
Charles Koehl <charles@etherscapes.com>
2022-10-14 15:44:48 -1000

    chore: debug

--- instant-roofer.php
+++ instant-roofer.php
@@ -3,7 +3,7 @@
 * Plugin Name:       Instant Roofer Booking Engine
 * Plugin URI:        https://instantroofer.com/integrations/wordpress-plugin
 * Description:       Embed the Instant Roofer Booking Engine on your WP site.
-* Version:           1.10.49
+* Version:           1.10.50
 * Requires at least: 5.2
 * Requires PHP:      7.2
 * Author:            Charles Koehl
@@ -14,7 +14,7 @@
 * Domain Path:       /languages
 */
 
-//require_once('custom-settings-page.php');
+require_once('custom-settings-page.php');
 
 const ANCHORS = [
     'online roof quote',
@@ -44,7 +44,44 @@ function callback_for_setting_up_scripts() {
  */
 function instantroofer_shortcode() {
 
-    return 'debugging';
+    $settings = get_option('instantroofer_options');
+
+    $accountId = $settings['instantroofer_field_account_id'];
+
+    if(!$accountId) {
+        return <<<STR
+            <p>Your Instant Roofer account ID is missing or invalid in the plugin settings.</p>
+STR;
+    }
+    $anchorText = get_anchor_text($accountId);
+    $width = $settings['instantroofer_field_width'];
+    $widthAttr = $width.'px';
+    $height = $settings['instantroofer_field_height'];
+    $heightAttr = $height.'px';
+    $spinnerUrl = plugins_url('assets/Iphone-spinner-2.gif', __FILE__);
+
+    $iframeQueryStringVals = array(
+        'id' => $accountId,
+        'fontFamily' => $settings['instantroofer_field_font_family'],
+    );
+
+    $iframeQueryString = http_build_query($iframeQueryStringVals);
+
+	return <<<STR
+        <div
+            class="instantroofer-container"
+            style="width: $widthAttr; height: $heightAttr; background-image: url('$spinnerUrl'); background-repeat: no-repeat; background-position: center;"
+        >
+            <iframe
+                id="instantroofer-iframe"
+                title="Instant Roofer Booking Engine"
+                src="https://book.instantroofer.com?$$iframeQueryString"
+                width="$widthAttr"
+                height="$heightAttr"
+            ></iframe>
+            <p><a href="https://instantroofer.com" target="_blank">$anchorText</a></p>
+        </div>
+STR;
 }
 
 /**
